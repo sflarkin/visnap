d1018de9b7f72c8adcfd66b42d6cd96eaef5c111
Miguel Rocha <rocham@uci.edu>
2013-12-02 14:05:52 -0800

    Added funtion to find the particle mass and calculate the optimal force resolution of a simulated dark matter halo

--- __init__.py
+++ __init__.py
@@ -7,6 +7,7 @@ VISNAP is being developed by the UC Irvine cosmology group, for questions
 contact: Miguel Rocha - rocham@uci.edu, Shea Garrison-Kimmel - sgarriso@uci.edu
 or Jose Onorbe - jonorbeb@uci.edu.
 '''
+import numpy as np
 
 # Define belwow any parameters that are to be used package-wide,
 # they are available to all VISNAP modules by calling visnap.parameter
@@ -16,14 +17,21 @@ or Jose Onorbe - jonorbeb@uci.edu.
 UnitLength_in_cm = 3.085678e21  # kpc
 UnitMass_in_g = 1.989e33 # Msun
 
-#PHYSICS AND COSMOLOGY (WMAP7)
+#PHYSICS
 
 G = 4.30071e-6 # [Msun^-1 kpc (km/s)^2]
-omega_matter = 0.266
-rho_b = 73.83  #background density at z=0 [Msun/kpc^3 h^2]
-rho_crit = rho_b/omega_matter # at z=0 
-delta_ovdens = 365.7 # at z=0
-h = 0.71
+
+#COSMOLOGY (WMAP7+BAO+H0, Komatsu et al. 2011) 
+
+h = 0.702
+omega_matter = 0.272
+omega_lambda = 0.728
+omega_baryons = 0.0455
+ns = 0.961
+sigma_8 = 0.807
+rho_crit = 3.0*(100.0/1000.0)**2/(8.0*np.pi*G) # at z=0 [Msun/kpc^3 h^2]
+rho_b = rho_crit*omega_matter  # background density at z=0  [Msun/kpc^3 h^2]
+delta_ovdens = 360.6 # at z=0
 dh = 1.9e-5
 keq = 0.0732*omega_matter*h**2
 
--- functions/mis.py
+++ functions/mis.py
@@ -3,19 +3,82 @@
 from numpy import *
 import visnap
 
+def particle_mass(box_size, res_level):
+    '''
+    Find the particle mass in a simulation
+
+    Input:
+
+     box_size - 1d side lenght of simulation box in [Mpc/h] 
+
+     res_level - Resolution level, i.e 2^(res_level) = Nparticles^(1/3)
+     
+    Output:
+    
+      mdark, mgas - Mass of dark matter and gas particles in [Msun/h]
+    '''
+    
+    rho_b = visnap.rho_b
+    Ob = visnap.omega_baryons
+    mp = (rho_b*(box_size*1000.0)**3)/2.0**(3.0*res_level)
+    mgas = mp*Ob
+
+    mdark = mp-mgas
+    return mdark, mgas
+
+
+def optimal_resolution(box_size, res_level, Mvir, Rvir, dark_matter_only=True):
+    '''
+    Find the optimal resolution to simulate a given dark matter halo following
+    power et al. 2003 criterion, i.e. opt_res = 0.5*4*R200/sqrt(N200)~1.6 Rvir/sqrt(Nvir)
+
+    Input:
+    
+     box_size - 1d side lenght of simulation box in [Mpc/h]
+
+     res_level - Resolution level, i.e 2^(res_level) = Nparticles^(1/3)
+
+     Mvir - Virial mass of dark matter halo in [Mpc/h]
+
+     Rvir - Virial radius of dark matter halo
+
+     dark_matter_only - Set to false if the simulation includes baryons
+
+    Output:
+    
+     opt_res - optimal resolution in [same units as the given Rvir]   
+    '''
+
+    mdark, mgas = particle_mass(box_size, res_level)
+    if dark_matter_only:
+        mp = mdark+mgas
+    else:
+        mp = mdark
+
+    print mp    
+    opt_res = 1.6*(Rvir/sqrt(Mvir/mp))
+    return opt_res
+
+    
 def find_rpower(r, nenc, avg_dens):
     '''
     Find the radius at which the numerical two body scattering time-scale is
     comparable to the Hubble time.  Eq. 20 in power et al. 2003 is used.
 
     Input:
+
      r - radial bins
+
      nenc - number of enclosed particles at r
+
      avg_dens - average enclosed density at r
 
     Output:
+
      r_power - minimum resolved radius
+
      argRes - r[argRes] gives all r > r_power  
+
      argNotRes - r[argNotRes] gives all r < r_power
     '''
     
@@ -32,7 +95,9 @@ def straight_line(x, x_cross, y_cross, dx=None, dy=None, slope=1):
     defined by x_cross, y_cross and slope (slope defaults to 1). 
     If dx and dy are given then slope = dy/dx.
     '''
+
     if (dx!=None) & (dy!=None): slope = dy/dx
     a = y_cross-slope*x_cross
     return a + slope*x
+
 
